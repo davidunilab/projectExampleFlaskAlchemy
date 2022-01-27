43c4035b0a280b2e6c218ec02f0bd99682f88050
David <davidchincharashvili@gmail.com>
2022-01-27 19:43:11 +0400

    addeb posts

---
+++ Models/user.py
@@ -0,0 +1,11 @@
+from main import db
+
+
+class UserModel(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
--- main.py
+++ main.py
@@ -1,48 +1,118 @@
-from flask import Flask, jsonify, request
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Resource, Api, reqparse, fields, marshal_with
 
-from flask_jwt_extended import create_access_token
-from flask_jwt_extended import get_jwt_identity
-from flask_jwt_extended import jwt_required
-from flask_jwt_extended import JWTManager
-
 app = Flask(__name__)
 api = Api(app)
-
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'Our-secret-key'
-
-
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #ar gvinda modifikaciebis trrackingi
 db = SQLAlchemy(app)
-jwt = JWTManager(app)
 
-resource_fields = {
+
+resource_fiels = {            #stringad rom gadaiqces rata postmenma waikitxos user getshi
     "id": fields.Integer,
     "username": fields.String,
-    "email": fields.String,
+    "email": fields.String
+}
+resource_posts = {
+    "id": fields.Integer,
+    "title": fields.String,
+    "body": fields.String,
+    "user_id": fields.Integer
 }
 
-class Auth(Resource):
-    def post(self):
-        username = request.json.get("username", None)
-        password = request.json.get("password", None)
-        if username != "test" or password != "test":
-            return jsonify({"msg": "Bad username or password"}), 401
+userparser = reqparse.RequestParser()
+userparser.add_argument("username", type=str, help='user_name must be string')
+userparser.add_argument("email", type=str, help='Email must be string')
 
-        access_token = create_access_token(identity=username)
-        return jsonify(access_token=access_token)
+postparser = reqparse.RequestParser()
+postparser.add_argument("id", type=int, help='Id must be integer')
+postparser.add_argument("title", type=str, help='Title must be string')
+postparser.add_argument("body", type=str, help='Body must be string')
+postparser.add_argument("user_id", type=int, help='User_id must be integer')
+class Post(Resource):
+    @marshal_with(resource_posts)
+    def get(self, post_id):
+        if post_id == 000:
+            return PostModel.query.all()
+        args = postparser.parse_args()
+        post = PostModel.query.filter_by(id=post_id).first()
+        return post
+
+    @marshal_with(resource_posts)
+    def post(self, post_id):
+        args = postparser.parse_args()
+        post = PostModel(title=args["title"], body=args["body"],user_id=args["user_id"])
+        db.session.add(post)
+        db.session.commit()
+        return "Item has been inserted!"
+
+    @marshal_with(resource_posts)
+    def put(self, post_id):
+        args = postparser.parse_args()
+        post = PostModel.query.filter_by(id=post_id).first()
+        if post == None:
+            post = PostModel(title=args["title"], body=args["body"], user_id=args["user_id"])
+        else:
+            post.title = args["title"]
+            post.body = args["body"]
+            post.user_id = args["user_id"]
+        db.session.add(post)
+        db.session.commit()
+        return "updated"
+
+    # @marshal_with(resource_posts)
+    def delete(self, post_id):
+        post = PostModel.query.filter_by(id=post_id).first()
+        db.session.delete(post)
+        db.session.commit()
+        return f"Post with id {post_id} has been deleted"
+
+class User(Resource):
+    @marshal_with(resource_fiels)
+    def get(self, user_id):
+        if user_id == 999:
+            return UserModel.query.all()
+        args = userparser.parse_args()
+        user = UserModel.query.filter_by(id=user_id).first()
+        return user
+
+    @marshal_with(resource_fiels)
+    def post(self, user_id):
+        args = userparser.parse_args()
+        user = UserModel(username=args["username"], email=args["email"])
+        db.session.add(user)
+        db.session.commit()
+        return "inserted"
+
+    @marshal_with(resource_fiels)
+    def put(self, user_id):
+        args = userparser.parse_args()
+        user = UserModel.query.filter_by(id=user_id).first()
+        if user == None:
+            user = UserModel(username=args["username"], email=args["email"])
+        else:
+            user.username = args["username"]
+            user.email = args["email"]
+        db.session.add(user)
+        db.session.commit()
+        return "updated"
+
+    # @marshal_with(resource_fiels)
+    def delete(self, user_id):
+        user = UserModel.query.filter_by(id=user_id).first()
+        db.session.delete(user)
+        db.session.commit()
+        return f"User with id {user_id} has been deleted"
 
 class UserModel(db.Model):
-    __tablename__ = 'users'
+    __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
-
+        return f"User {self.username}"
 
 class PostModel(db.Model):
     __tablename__ = 'posts'
@@ -52,74 +122,12 @@ class PostModel(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
     def __repr__(self):
-        return '<Post %r>' % self.title
-
+        return f"Post {self.title}"
+# db.create_all()
 # quit()
-
-
-
-userParser = reqparse.RequestParser()
-userParser.add_argument("username", type=str, help="user name should be string")
-userParser.add_argument("email", type=str, help="Email should be string")
-
-
-class Post(Resource):
-    def get(self, post_id):
-        pass
-
-    def post(self, post_id):
-        pass
-
-    def put(self, post_id):
-        pass
-
-    def delete(self, post_id):
-        pass
-
-
-# CRUD
-class User(Resource):
-    @marshal_with(resource_fields)
-    @jwt_required()
-    def get(self, user_id):
-        if user_id == 999:
-            return UserModel.query.all()
-        user = UserModel.query.filter_by(id=user_id).first()
-        return user
-
-    @marshal_with(resource_fields)
-    def post(self, user_id):
-        args = userParser.parse_args()
-        user = UserModel(username=args["username"], email=args["email"])
-        db.session.add(user)
-        db.session.commit()
-        return "inserted"
-
-    @marshal_with(resource_fields)
-    def put(self, user_id):
-        args = userParser.parse_args()
-        user = UserModel.query.filter_by(id=user_id).first()
-        if user == None:
-            user = UserModel(username=args["username"], email=args["email"])
-        else:
-            user.username = args["username"]
-            user.email = args["email"]
-
-        db.session.add(user)
-        db.session.commit()
-        return "edited"
-
-    def delete(self, user_id):
-        user = UserModel.query.filter_by(id=user_id).first()
-        db.session.delete(user)
-        db.session.commit()
-
-        return f"deleted user with id {user_id}"
-
 
 api.add_resource(User, '/user/<int:user_id>')
 api.add_resource(Post, '/post/<int:post_id>')
-api.add_resource(Auth, '/login')
 
 
 if __name__ == "__main__":
